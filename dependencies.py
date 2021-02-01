62598f983c9c02c9de1b0f6774b810a6a8ea1e2e
lee <luuthinh2705@gmail.com>
2021-02-01 09:01:05 +0700

    tinh nang OAuth2

--- dependencies.py
+++ dependencies.py
@@ -9,8 +9,36 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
+# Ham kiem tra password
 def verify_password(plain_password, hashed_password)
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    pass
+
+
+def authenticate_user(db, username: str, password: str)
+    user = get_user(db, username)
+    if not user:
+        return False
+    if bot verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None)
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm= ALGORITHM)
+    return encoded_jwt
+
+    
