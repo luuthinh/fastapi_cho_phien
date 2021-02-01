7134ba28e26440df0754ddad6cb3f56ace613dea
lee <luuthinh2705@gmail.com>
2021-02-01 08:58:00 +0700

    tinh nang OAuth2

--- dependencies.py
+++ dependencies.py
@@ -1,11 +1,16 @@
-from fastapi import Header, HTTPException
+from datetime import datetime, timedelta
+from typing import List, Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+# openssl rand -hex 32
+SECRET_KEY = "80d6f7b735d046d01bee7615af37e62af64c84666a19fb0f1fe90917637903bb"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-async def get_toker_header(x_token: str = Header(...)):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+def verify_password(plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-async def get_query_token(token: str):
-    if token != 'jessica':
-        raise HTTPException(status_code=400, detail="No Jessica token provided")
+def
