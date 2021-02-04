5971888fe0c5175080ce814dc5b458baf120df05
lee <luuthinh2705@gmail.com>
2021-02-04 09:43:37 +0700

    them core

--- core/config.py
+++ core/config.py
@@ -1,10 +1,24 @@
 import os
 
 from dotenv import load_dotenv
+from starlette.datastructures import CommaSeparatedStrings, Secret
 from databases import DatabaseURL
 
-MONGODB_URL = os.getenv("MONGODB_URL", "")
+API_V1_STR = '/api'
 
+JWT_TOKEN_PREFIX = "Token"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+
+load_dotenv(".env")
+
+MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT",10))
+MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT",10))
+SECRET_KEY = Secret(os.getenv("SECRET_KEY", "9c70d761070b842e2fe3d405de5dd92f1c0ed553865e06faa60652806038519f"))
+
+PROJECT_NAME = os.getenv("PROJECT_NAME"," Cho phien")
+ALLOWED_HOSTS = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", ""))
+
+MONGODB_URL = os.getenv("MONGODB_URL", "")
 if not MONGODB_URL:
     MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
     MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
@@ -16,5 +30,11 @@ if not MONGODB_URL:
 else:
     MONGO_URL = DatabaseURL(MONGODB_URL)
 
-print("ppp", MONGODB_URL)
-database_name = MONGO_DB    
+
+database_name = MONGO_DB
+article_collection_name = "articles"
+favorites_collection_name = "favorites"
+tags_collection_name = "tags"
+users_collection_name = "users"
+comments_collection_name = "commentaries"
+followers_collection_name = "followers"    
